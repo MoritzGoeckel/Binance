c8f39c62a1564d0e4ca221cbd08acf3acd157e98
Moritz Göckel <moritz.goeckel@gmail.com>
2024-07-28 22:28:01 +0200

    Delete V0 lib

--- Download.py
+++
@@ -1,9 +0,0 @@
-from Lib import loadTradesDict, startDownload, makeTime
-
-pair = "PEPEUSDT"
-trades = loadTradesDict(pair)
-smallestId = None if len(trades) == 0 else min(trades.keys())
-if smallestId != None:
-    print(f"Smallest id: {smallestId} @ {makeTime(trades[smallestId]['time'])}")
-
-startDownload(pair, smallestId)
